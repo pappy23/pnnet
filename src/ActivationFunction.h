ebfcd125fc2ddc2e22d7f6d86db446926313751c
yashin.vladimir <yashin.vladimir@4f982616-068c-11de-a6e9-d320b29efae0>
2009-03-05 13:27:56 +0000

    Added a couple of classes

--- src/ActivationFunction.h
+++ src/ActivationFunction.h
@@ -3,14 +3,31 @@
 #ifndef ACTIVATIONFUNCTION_H
 #define ACTIVATIONFUNCTION_H
 
-#include "Object.h"
-
 namespace pann
 {
-
-    class ActivationFunction : public Object
+    namespace ActivationFunctions
     {
-    };
+        class Base
+        {
+        public:
+            static float f(float);
+            static float derivative(float);
+        };
+
+        class Linear : public Base
+        {
+        public:
+            static float f(float x)
+            {
+                return x;
+            } //f
+
+            static float derivative(float)
+            {
+                return 0;
+            } //derivative
+        };
+    }; //ActivationFunctions
 
 }; //pann
 #endif //ACTIVATIONFUNCTION_H
