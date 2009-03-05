a8ffb5bf92ef3d58ee3a4f96fe6fdd3e0e0bad90
yashin.vladimir <yashin.vladimir@4f982616-068c-11de-a6e9-d320b29efae0>
2009-03-05 22:14:21 +0000

    1

--- src/Neuron.h
+++ src/Neuron.h
@@ -24,6 +24,24 @@ namespace pann
             activationValue(activationFunction.f(0)) { };
 
         ~Neuron() { };
+
+        int connectTo(Neuron & to, Weight & w)
+        {
+            //if(links.find(link with to == to))
+            //    throw E("Achtung!");
+            Link link_to(to, w, Link::Direction::out);
+            links.push_back(link_to);
+
+            Link link_from(*this, w, Link::Direction::in);
+            to.links.push_back(link_from);
+
+            return 0;
+        };
+
+        int connectFrom(Neuron & from, Weight & w)
+        {
+            return from.connectTo(*this, w);
+        };
     };
 
 }; //pann
