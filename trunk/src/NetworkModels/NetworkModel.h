b03a2c5e9764147bccff270e16f5aa6760343566
yashin.vladimir <yashin.vladimir@4f982616-068c-11de-a6e9-d320b29efae0>
2009-03-15 15:38:21 +0000

    fixed TanH static const; added some NetworkModels and LearningAlgorithms stubs

---
+++ trunk/src/NetworkModels/NetworkModel.h
@@ -0,0 +1,20 @@
+/**
+ * @file
+ * Abstract for all network models
+ */
+
+#ifndef NETWORKMODEL_H
+#define NETWORKMODEL_H
+
+#include "Includes.h"
+
+namespace pann
+{
+    class NetworkModel
+    {
+    };
+
+}; //pann
+
+#endif
+
