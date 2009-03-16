0206ba9602a7fcbbb20a71924079bca3e5692025
yashin.vladimir <yashin.vladimir@4f982616-068c-11de-a6e9-d320b29efae0>
2009-03-16 22:55:03 +0000

    1

--- src/Utils/pann_viewer/main.cpp
+++ src/Utils/pann_viewer/main.cpp
@@ -1,10 +1,37 @@
 #include <QApplication>
+#include <QMessageBox>
 
 #include "window.h"
+
+#include "Net.h"
+#include "Storage.h"
+
+using namespace pann;
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
+    QMessageBox err_msg;
+
+    if(argc != 2)
+    {
+        //TODO replace pann_viewer with argv[0]
+        err_msg.setText("Usage: pann_viewer <filename>");
+        err_msg.exec();
+        return -1;
+    }
+
+    Net net;
+    try {
+        Storage::load(net, argv[1]);
+    } catch(Exception::Base& e) {
+        err_msg.setText(e.getText().c_str());
+        err_msg.exec();
+        return -1;
+    }
+
+    //Build GL
+
     Window window;
     window.show();
     return app.exec();
