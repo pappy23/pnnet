ff6276f1bd12611a67b642d44b8404f39e785866
yashin.vladimir <yashin.vladimir@4f982616-068c-11de-a6e9-d320b29efae0>
2009-03-23 21:22:33 +0000

    added TestLms

---
+++ src/Core/pann.h
@@ -0,0 +1,18 @@
+/**
+ * Library public interface
+ */
+
+#ifndef PANN_H
+#define PANN_H
+
+#include "Includes.h"
+#include "Type.h"
+#include "Net.h"
+#include "LMS.h"
+#include "Storage.h"
+#include "TrainData.h"
+#include "NetworkModel.h"
+#include "Moo.h"
+
+#endif
+
