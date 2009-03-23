ece44ee3605b17760edefd12f5c450b7a598b8fd
yashin.vladimir <yashin.vladimir@4f982616-068c-11de-a6e9-d320b29efae0>
2009-03-23 21:22:33 +0000

    added TestLms

---
+++ trunk/src/UnitTests/TestLms.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+#include "pann.h"
+
+using namespace std;
+using namespace pann;
+using namespace boost;
+
+int main()
+{
+    //Constructing perceptron
+    vector<unsigned> layers;
+    layers.push_back(1); //input
+    for(unsigned i = 0; i < 1; ++i)
+        layers.push_back(2);
+    layers.push_back(1); //output
+    Net* net = NetworkModel::MultilayerPerceptron(layers, ActivationFunction::TanH::Instance());
+
+    //Data
+    TrainData td;
+    for(unsigned i = 0; i < 10; ++i)
+    {
+        TrainPattern tp(1, 1);
+        tp.input[0] = i;
+        tp.desired_output[0] = -i;
+        td.data.push_back(tp);
+    }
+
+    for(unsigned i = 0; i < 10; ++i)
+    {
+        td.shuffle();
+        LMS::init(*net);
+        LMS::train(*net, td);
+        cout<<"E="<<td.getMse()<<endl;
+    }
+
+    //Storage::save(*net, "test_lms.xml");
+
+    Net* n2 = new Net;
+    Storage::load(*n2, "test_lms.xml");
+
+    return 0;
+}
+
