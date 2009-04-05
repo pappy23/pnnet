d6a822e09c3d8626bfb28241b16d33d5dcc20164
yashin.vladimir <yashin.vladimir@4f982616-068c-11de-a6e9-d320b29efae0>
2009-04-05 10:19:43 +0000

    1

--- src/UnitTests/TestLms.cpp
+++ src/UnitTests/TestLms.cpp
@@ -10,7 +10,7 @@ void test(Net* net, Float start, Float stop, Float step);
 
 Float func(Float _x)
 {
-    return _x * _x / 50.0;
+    return _x * _x;
 }
 
 int main()
@@ -18,9 +18,10 @@ int main()
 //*
     //Constructing perceptron
     vector<unsigned> layers;
-    layers.push_back(1); //input
-    layers.push_back(50);
-    layers.push_back(1); //output
+    layers.push_back(1); //input  - no act. fcn
+    layers.push_back(3); //hidden - tanh
+    layers.push_back(2); //hidden - tanh
+    layers.push_back(1); //output - linear
     Net* net = NetworkModel::MultilayerPerceptron(layers, ActivationFunction::TanH::Instance());
 
     //Data
@@ -28,13 +29,13 @@ int main()
     //boost::function<Float (Float)> f = boost::bind( (Float (*)(Float))func, _1);
 
     //Learning
-    const unsigned epochs = 5;
+    const unsigned epochs = 1000;
     vector<Float> train_error_info; //MSE
 
-    TrainData& td = *(DataGenerator::generateFromFunction(-3.5, +3.5, 100, func));
+    TrainData& td = *(DataGenerator::generateFromFunction(-1.0, +1.0, 10, func));
 
     Lms::init(*net);
-    net->learningHint[LmsAttributes::learningRate] = 0.1;
+    net->learningHint[LmsAttributes::learningRate] = 0.2;
     net->learningHint[LmsAttributes::learningMomentum] = 0.5;
     
     boost::progress_display progress(epochs);
@@ -49,11 +50,13 @@ int main()
         Lms::train(*net, td);
         
         train_error_info.push_back(td.getMse());
-        //cout<<td.getMse()<<endl;
+//        cout<<td.getMse()<<endl;
+//        if(i % (epochs / 5 + 1) == 0)
+  //          test(net, -1.0, +1.0, +0.01);
     }
     }
 
-    test(net, -3.5, +3.5, +0.01);
+    test(net, -2.0, +2.0, +0.01);
 
     //Save trained net
     Storage::save(*net, "test_lms.xml");
