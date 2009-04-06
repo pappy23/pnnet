fb19c1b76a7d744762a330b194858d004c2a6a05
yashin.vladimir <yashin.vladimir@4f982616-068c-11de-a6e9-d320b29efae0>
2009-04-06 18:59:04 +0000

    article 1

---
+++ trunk/src/UnitTests/Article1_learning.cpp
@@ -0,0 +1,101 @@
+#include <iostream>
+
+#include "pann.h"
+
+using namespace std;
+using namespace pann;
+using namespace boost;
+
+void test(Net* net, Float start, Float stop, Float step);
+
+Float func(Float _x)
+{
+    return sin(_x);
+}
+
+int main()
+{
+//*
+    //Constructing perceptron
+    vector<unsigned> layers;
+    layers.push_back(1); //input  - no act. fcn
+    layers.push_back(9); //hidden - tanh
+    layers.push_back(4); //hidden - tanh
+    layers.push_back(1); //output - linear
+    Net* net = NetworkModel::MultilayerPerceptron(layers, ActivationFunction::TanH::Instance());
+
+    //Learning
+    const unsigned epochs = 500;
+    TrainData& td = *(DataGenerator::generateFromFunction(-3.0, +3.0, 20, func));
+
+    Lms::init(*net);
+    net->learningHint[LmsAttributes::learningRate] = 0.1;
+    net->learningHint[LmsAttributes::learningMomentum] = 0.5;
+    
+    vector<Float> train_error_info; //MSE
+    for(unsigned i = 1; i < epochs; ++i)
+    {
+        td.shuffle();
+        Lms::train(*net, td, 2);
+        
+        train_error_info.push_back(td.getMse());
+    }
+
+    test(net, -4.0, +4.0, +0.01);
+
+    //Save trained net
+    Storage::save(*net, "test_lms.xml");
+
+    //Plotting error graph
+    try {
+        Gnuplot gp_err("lines");
+        gp_err.set_title("Error by epoch");
+        gp_err.plot_x(train_error_info);
+
+        cout<<"Press ENTER to exit...";
+        cin.get();
+
+    } catch(GnuplotException e) {
+        cout << e.what() << endl;
+    }
+    
+    return 0;
+}
+
+void test(Net* net, Float start, Float stop, Float step)
+{
+    //Test
+    vector<Float> input, output, desired_output, error;
+
+    unsigned i = 0;
+    for(Float x = start; x < stop; x += step, ++i)
+    {
+        TrainPattern tmp(1, 1);
+        tmp.input[0] = x;
+        tmp.desired_output[0] = func(x);
+
+        net->setInput(tmp.input);
+        net->run(FeedforwardPropagationRunner::Instance());
+        net->getOutput(tmp.error); //actual output
+
+        input.push_back(x);
+        desired_output.push_back(func(x));
+        output.push_back(tmp.error[0]);
+        error.push_back(tmp.desired_output[0] - tmp.error[0]);
+    }
+
+    try {
+        Gnuplot gp_test("points");
+        gp_test.set_title("Test");
+        gp_test.set_smooth().plot_xy(input, error, "Error");
+        gp_test.set_smooth().plot_xy(input, desired_output, "Desired output");
+        gp_test.set_smooth().plot_xy(input, output, "Output");
+
+        cout<<"Press ENTER to exit...";
+        cin.get();
+
+    } catch(GnuplotException e) {
+        cout << e.what() << endl;
+    }
+
+}; //test
