e180f5a3d43242279798a08e1f4d53714877386e
yashin.vladimir <yashin.vladimir@4f982616-068c-11de-a6e9-d320b29efae0>
2009-04-13 21:49:48 +0000

    ConvolutionNetworkDraft

---
+++ trunk/src/pann/NetworkModels/ConvolutionalNetwork.h
@@ -0,0 +1,17 @@
+//ConvolutionalNetwork.h
+
+#include <vector>
+
+#include "Core/ActivationFunction.h"
+
+namespace pann
+{
+    class Net;
+
+    namespace NetworkModel
+    {
+        Net& ConvolutionalNetworkDraft();
+    };
+
+}; //pann
+
