61d0b11297d2340eab5f55364cce7a1b95be6a5f
yashin.vladimir <yashin.vladimir@4f982616-068c-11de-a6e9-d320b29efae0>
2009-04-20 08:02:06 +0000

    1

--- trunk/src/Apps/UnitTests/TestConvolutionalNetwork.cpp
+++ trunk/src/Apps/UnitTests/TestConvolutionalNetwork.cpp
@@ -11,9 +11,10 @@ using namespace boost;
 
 int main()
 {
-    //Net& net = NetworkModel::ConvolutionalNetworkDraft();
+    Net& net = NetworkModel::ConvolutionalNetworkDraft();
 
     //Constructing perceptron
+    /*
     vector<unsigned> layers;
     layers.push_back(1000);
     layers.push_back(1000);
@@ -21,6 +22,7 @@ int main()
     layers.push_back(1000);
     layers.push_back(1000);
     Net& net = NetworkModel::MultilayerPerceptron(layers, ActivationFunction::TanH::Instance());
+    */
 
     Storage::save<Storage::txt_out>(net, "test_conv.bin");
 
