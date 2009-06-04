0d78db80382db7850666b25886ef4abdfcff9bf3
yashin.vladimir <yashin.vladimir@4f982616-068c-11de-a6e9-d320b29efae0>
2009-06-04 20:17:06 +0000

    writeImage() sceleton

--- trunk/src/pann/TrainData/ImageIo.h
+++ trunk/src/pann/TrainData/ImageIo.h
@@ -7,23 +7,35 @@
 #include "Image.h"
 #include "Squash.h"
 
+using std::string;
+
 namespace pann
 {
     class ImageIo
     {
     public:
+        enum FileType { PGM, PPM };
         /**
          * Reads image, determines it's format and calls corresponding reader
          * @param _filename Image file to read from
          * @return @see Image object
          */
-        static Image readImage(const std::string& _filename);
+        static Image readImage(const string& _filename);
+
+        /**
+         * Writes image to file
+         * @param _img Image to write
+         * @param _filename Filename
+         * @param _type File format (@see FileType)
+         */
+        static void writeImage(const Image& _img, const string& _filename, FileType _type);
 
     private:
         ImageIo();
         ~ImageIo();
 
-        static Image readP5P6(const std::string& _filename);
+        static Image readP5P6(const string& _filename);
+        static void writeP5(const Image& _img, const string& _filename);
     };
 
 }; //pann
