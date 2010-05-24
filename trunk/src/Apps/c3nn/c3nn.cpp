1a3a8c85900bde333d7ae0d1db36613e6032997e
yashin.vladimir <yashin.vladimir@4f982616-068c-11de-a6e9-d320b29efae0>
2010-05-24 17:06:56 +0000

    Oops! Forgot to sync c3nn.cpp

--- trunk/src/Apps/c3nn/c3nn.cpp
+++ trunk/src/Apps/c3nn/c3nn.cpp
@@ -16,29 +16,7 @@ int main(int argc, char ** argv)
 
     net->run(FeedforwardPropagationRunner::Instance());
 
-    const NetCache & cache = net->get_cache();
-    cout<<"Cache layers["<<cache.layers.size()<<"] : ";
-    for(unsigned i = 0; i < cache.layers.size(); ++i)
-        cout<<cache.layers[i].size()<<" ";
-    cout<<endl;
 
-    /*
-    cout<<"Tracing:\n";
-    for(unsigned layer_no = 0; layer_no < cache.layers.size(); ++layer_no)
-    {
-        cout<<"Layer "<<layer_no<<'\n';
-        for(unsigned neuron_no = 0; neuron_no < cache.layers[layer_no].size(); ++neuron_no)
-        {
-            NeuronPtr n = cache.layers[layer_no][neuron_no];
-            cout<<"uid:\t"<<n->get_attr(pann::hash("uid"))<<"\nlayer:\t"<<n->get_attr((pann::hash("layer")))<<
-                    "\nplane:\t"<<n->get_attr(pann::hash("plane"))<<"\nx:\t"<<n->get_attr(pann::hash("x"))<<
-                    "\ny:\t"<<n->get_attr(pann::hash("y"))<<"\n";
-            BOOST_FOREACH( const Link & link, n->input_links )
-                    cout<<"-> "<<link.get_to()->get_attr(pann::hash("uid"))<<", w = "<<link.get_weight()->get_attr(pann::hash("uid"))<<'\n';
-            cout<<endl;
-        };
-    };
-    */
 
     return 0;
 }; //main
