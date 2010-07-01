902aa1a6cea87d34853b7f93be4fb1ea98fca63f
yashin.vladimir <yashin.vladimir@4f982616-068c-11de-a6e9-d320b29efae0>
2010-07-01 18:36:37 +0000

    c3nn configs

--- trunk/src/Apps/c3nn/c3nn.cpp
+++ trunk/src/Apps/c3nn/c3nn.cpp
@@ -8,15 +8,68 @@ using namespace boost;
 using namespace boost::assign;
 using namespace pann;
 
+class NetConfigT
+{
+public:
+    NetConfigT() : layers(5) {};
+    virtual ~NetConfigT() {};
+
+    int layers;
+
+    /* Serialization */
+private:
+    friend class boost::serialization::access;
+    template<class Archive>
+        void serialize(Archive & ar, const unsigned int version)
+        {
+            ar & BOOST_SERIALIZATION_NVP(var);
+        };
+}; //NetConfigT
+
+class ParentConfigT
+{
+public:
+    ParentConfigT() {};
+    virtual ~ParentConfigT() {};
+
+public:
+    vector<NetConfigT> nets;
+
+    /* Serialization */
+private:
+    friend class boost::serialization::access;
+    template<class Archive>
+        void serialize(Archive & ar, const unsigned int version)
+        {
+            ar & BOOST_SERIALIZATION_NVP(children);
+        };
+}; //ParentConfigT
+
+void save_config(const ParentConfigT & config, const string & filename)
+{
+    typedef boost::archive::xml_oarchive FormatT;
+
+    ofstream ofs(filename.c_str());
+    if(ofs.fail())
+        throw IoError()<<"save_config: failed to open file "<<filename<<" for writing\n"; 
+
+    FormatT oa(ofs);
+    try {
+        oa << BOOST_SERIALIZATION_NVP(config);
+    } catch(boost::archive::archive_exception& e) {
+        throw IoError()<<"save(): failed to save config. Boost exception thrown. What: "<<e.what()<<"\n";
+    } catch(...) {
+        throw IoError()<<"save(): unknown exception\n";
+    }
+
+    ofs.close();
+}; //save_config
+
 int main(int argc, char ** argv)
 {
-    vector<unsigned> layers;
-    layers += 1,2;
-    NetPtr net = make_convnet(layers, 1.0, 2, 2, 1, 1, Linear::Instance(), TanH::Instance(), TanH::Instance(), TanH::Instance());
+    ParentConfigT config;
 
-    net->run(FeedforwardPropagationRunner::Instance());
-
-
+    save_config(config, "c3nn-cfg.xml");
 
     return 0;
 }; //main
